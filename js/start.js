481d40b72cb4d28b4405f267299cf8c7bc467616
HAMZA EL ANSARI <hamza.elansari@gmail.com>
2023-09-06 19:22:21 +0100

    Exercicejs

---
+++ js/start.js
@@ -0,0 +1,45 @@
+
+//var a = parseInt( prompt("Saisir la valeur de a"));
+// var b =parseInt(prompt("Saisir la valeur de b"));
+// //console.log("a+b = "+(a+b));
+
+// b*=10;
+// console.log(b);
+// var entreprise="COcACOLA";
+// var s1="Bonjour tout le monde " + entreprise;
+// var s=`Bonjour Tout le monde ${entreprise} rzgrtzrt`;
+
+// console.log(s);
+// var lang=prompt("Saisir votre langue");
+// var lang2=prompt("Saisir votre deuxième langue langue");
+// if(lang=="fr" || lang2=="en"){
+//     console.log("Candidature acceptée");
+
+// }else{
+//     console.log("Candidature refusée");
+// }
+// switc = h(lang){
+//     case 'fr':
+//         case 'francais':
+//         console.log("Bonjour");
+//         break;
+//         case 'en':
+//             case 'english':
+//                 case 'anglais':
+//             console.log("Hello");
+//             break;
+//             case 'ar':
+//                 case "arabe":                    
+//                 console.log("Marhaba");
+//                 break;
+// }
+
+function Nomdelafonction(){
+    alert("Bonjour tout le monde depuis la fonction nomdelafonction");
+}
+
+function Somme(a,b){
+    return parseInt(a)+parseInt(b);
+}
+
+alert("LA somme est  "+ Somme(prompt("Saisir a"),prompt("Saisir b")));
